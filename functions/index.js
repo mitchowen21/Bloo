5043b02e9f130cd2af2867d4407bf98eca110514
mitchowen21 <mitchowen21@gmail.com>
2018-06-24 16:59:08 -0500

    Begin Dnsimple Integration

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,54 @@
 const functions = require('firebase-functions');
+const express = require('express');
+const cors = require('cors')({ origin: true });
+const bodyParser = require('body-parser');
+const request = require('request');
+const dnsimple = require('dnsimple')({ baseUrl: 'https://api.sandbox.dnsimple.com', accessToken: functions.config().dnsimple.access });
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+/***********************************************************
+|
+| DNS Simple Integration
+|
+***********************************************************/
+
+exports.checkDomainAvailability = functions.https.onRequest((req, res) => {
+	cors(req, res, () => {
+		if(req.method == 'POST') {
+			checkAuthorization().then((response) => {
+				var accountId = response;
+				var domainName = req.body.domainName;
+				dnsimple.registrar.checkDomain(accountId, domainName).then((response) => {
+					res.status(200).send(response);
+				}).catch(error => {
+					res.status(500).send(error);
+				});
+			}).catch(error => (
+				res.status(500).send(error)
+			));
+		} else {
+			res.status(405).send('Method Not Allowed');
+		}
+	});
+});
+
+var checkAuthorization = function() {
+	return new Promise(function(resolve, reject){
+		dnsimple.identity.whoami().then((response) => {
+			var accountID = response.data.account.id;
+			resolve(accountID);
+		}).catch((error) => {
+			reject(error);
+		});
+	});
+}
+
+
+/***********************************************************
+|
+| END DNS Simple Integration
+|
+***********************************************************/
